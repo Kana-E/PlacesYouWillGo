a5d54cd09e0c4eedd5c22f310af92dfd4bf7865e
Kana-E <kana.eguchi@yahoo.com>
2020-07-26 11:44:55 -0400

    sets up db and sequelize

---
+++ database/index.js
@@ -0,0 +1,44 @@
+const {Sequelize, Model} = require('sequelize');
+const Config = require('./config.json');
+
+const sequelize = new Sequelize(
+  Config.database,
+  Config.username,
+  Config.password,
+  {
+    host: Config.host,
+    dialect: 'mysql'
+  }
+);
+
+// const setUp = `CREATE DATABASE IF NOT EXISTS ${Config.database};`;
+
+sequelize.authenticate()
+  .then ( () => console.log('Connection has been established successfully.') )
+  .catch( error => console.error('Unable to connect to the database:', error) );
+
+
+class Plans extends Model {}
+
+Plans.init({
+  destination: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  destination: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  image: {
+    type: Sequelize.BLOB('long'),
+    allowNull: true
+  }
+  //pass connection instanse and model name
+}, { sequelize, moduelName: 'plans'} );
+
+Plans.sync()
+  .catch( err => console.error(err));
+
+
+module.exports.sequelize = sequelize;
+module.exports.Plans = Plans;
